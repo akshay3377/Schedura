6867cd301dad858bfd2f5ce38b4e6e482dba0338
akshay3377 <workwithakshay777@gmail.com>
2025-05-15 18:20:56 +0530

    fix

--- components/event-form.jsx
+++ components/event-form.jsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+
 import {
   Select,
   SelectContent,
